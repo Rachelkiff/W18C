a31e3ceb1782319b73bbd8a91fd61d4d6645843c
rachel <rachelkiffiak1@gmail.com>
2020-10-28 19:37:01 -0600

    Made the adjustments to the code to follow the assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,18 @@
-def getFizzBuzz(multiples, *args):
-    for i in range(*args):
-        output = ""
-        for multiple in multiples:
-            if i % multiple==0:
-                output+=multiples[multiple]
-        if output =="":
-            output = i
-        print(output)            
+def fizzbuzz(num):
+    if(num % 3 == 0 and num % 5):
+       print("Fizzbuzz")
+    elif(num % 3 == 0):
+        print("Fizz")
+    elif(num % 5 == 0):
+        print("Buzz")
+    else:
+        print(num)        
 
-multiples = {3:'Fizz', 5:'Buzz'}
-getFizzBuzz(multiples,101)
 
-multiples = [5,6,10,15,20,26,32,40,46,52,57,60,66,75,84,90]   
-for multiple in multiples:
-    print(multiple)
+
+
+
+nums = [5,6,10,15,20,26,32,40,46,52,57,60,66,75,84,90]   
+for num in nums:
+    fizzbuzz(num)
 
